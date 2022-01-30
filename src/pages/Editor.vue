1f25c8acfad7c189c8961aa963dd2dc69792d825
pukochka <tema.puko@gmail.com>
2022-01-31 00:22:39 +0300

    add link editot trello

---
+++ src/pages/Editor.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="q-pa-md q-gutter-sm">
+    <q-editor v-model="editor" min-height="5rem" />
+
+    <q-card flat bordered>
+      <q-card-section>
+        <pre style="white-space: pre-line">{{ editor }}</pre>
+      </q-card-section>
+    </q-card>
+
+    <q-card flat bordered>
+      <q-card-section v-html="editor" />
+    </q-card>
+  </div>  
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      
+      lorem: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, ratione eum minus fuga, quasi dicta facilis corporis magnam, suscipit at quo nostrum!',
+
+      stars: ref(3),
+      header: ref(''),
+      description: ref(''),
+      slideVol: ref(39),
+      slideAlarm: ref(56),
+      slideVibration: ref(63)
+    }
+  },
+  methods:{
+    clearValue(){
+      console.log(this.header,this.description)
+      this.header ='';
+      this.description ='';
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
